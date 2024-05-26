2c9fbdf93ce27290d90d39e019c83f30f3be46c1
Catlisan <letsboss228@gmail.com>
2024-05-26 16:13:45 +0600

    first commit

---
+++ DisableDamageOverlay/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+public static class MarseyPatch
+{
+    
+   
+    public static Assembly ContentClient = null;
+
+    public static string Name = "DamageOverlay Patch";
+    public static string Description = "Disable damage overlay by Catlisan";
+}
+
+[HarmonyPatch]
+public static class CombatOverlayPatch
+{
+    private static MethodBase TargetMethod() 
+    {
+        var CombatOverlay = MarseyPatch.ContentClient.GetType("Content.Client.UserInterface.Systems.DamageOverlays.Overlays.DamageOverlay")!;
+        return CombatOverlay.GetMethod("Draw", BindingFlags.NonPublic | BindingFlags.Instance);
+    }
+
+  
+    [HarmonyPrefix]
+    private static bool PrefSkip()
+    {
+        return false;
+    }
+}
